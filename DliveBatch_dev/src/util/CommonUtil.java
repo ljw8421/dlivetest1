e6fe8c86c8fa09c115e0fbf88fb9524a48b0afe0
kh <kh@khPC>
2018-04-19 15:23:29 +0900

    수정

---
+++ DliveBatch_dev/src/util/CommonUtil.java
@@ -0,0 +1,388 @@
+package util;
+
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+
+import com.oracle.xmlns.adf.svc.types.Conjunction;
+import com.oracle.xmlns.adf.svc.types.FindCriteria;
+import com.oracle.xmlns.adf.svc.types.ViewCriteria;
+import com.oracle.xmlns.adf.svc.types.ViewCriteriaItem;
+import com.oracle.xmlns.adf.svc.types.ViewCriteriaRow;
+
+public class CommonUtil {
+	
+	private static Logger logger = Logger.getLogger(CommonUtil.class);
+
+	/**
+	 * Sales Cloud 조회 시 검색에 필요한 Filter Setting
+	 */
+	public List<Map<String, Object>> addFilterList(String[] itemAttribute, String[] itemValue,
+			boolean[] upperCaseCompare, String[] operator) 
+	{
+		List<Map<String, Object>> filterList = null;
+		Map<String, Object> filterMap;
+
+		filterList = new ArrayList<>();
+
+		try {
+
+			if (itemAttribute.length == itemValue.length && itemAttribute.length == upperCaseCompare.length
+					&& itemAttribute.length == operator.length) 
+			{
+				if (itemAttribute.length != 0) 
+				{
+					for (int i = 0; i < itemAttribute.length; i++) 
+					{
+						if (!"".equals(itemValue[i]) || !isEmpty(itemValue[i]))
+						{
+							if("ISBLANK".equals(operator[i])) 
+							{
+								filterMap = new HashMap<>();
+								filterMap.put("itemAttribute", itemAttribute[i]);
+								filterMap.put("upperCaseCompare", isEmpty(upperCaseCompare[i]));
+								filterMap.put("itemOperator", operator[i]);
+
+								filterList.add(filterMap);
+							}
+							else if("BETWEEN".equals(operator[i]))
+							{
+								filterMap = new HashMap<>();
+								filterMap.put("itemAttribute", itemAttribute[i]);
+								filterMap.put("upperCaseCompare", isEmpty(upperCaseCompare[i]));
+								filterMap.put("itemOperator", operator[i]);
+
+								String values[] = itemValue[i].split(",");
+								filterMap.put("itemValue", values);
+								
+								logger.info("values : " + values[1]);
+								
+								filterList.add(filterMap);
+							}
+							else 
+							{
+								filterMap = new HashMap<>();
+								filterMap.put("itemAttribute", itemAttribute[i]);
+								filterMap.put("itemValue", itemValue[i]);
+								filterMap.put("upperCaseCompare", isEmpty(upperCaseCompare[i]));
+								filterMap.put("itemOperator", operator[i]);
+
+								filterList.add(filterMap);
+							}
+						}
+					}
+				}
+			} 
+			else {
+				Exception e = new Exception("배열의 길이 수가 일치 하지 않습니다.");
+				throw e;
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.info(e.getMessage());
+			e.printStackTrace();
+		}
+
+		return filterList;
+	}
+	
+	/**
+	 * Sales Cloud 조건 검색
+	 * */
+	public FindCriteria getCriteria(List<Map<String, Object>> filterList, Conjunction conjunction, String[] items,
+			int pageNum, int size) throws Exception 
+	{
+		int start = (pageNum - 1) * size;
+		logger.debug("start : " + start);
+
+		FindCriteria findCriteria = new FindCriteria();
+
+		findCriteria.setFetchStart(start);
+		findCriteria.setFetchSize(size);
+
+		if (filterList != null)
+		{
+			if (filterList.size() != 0) 
+			{
+				ViewCriteria filter = new ViewCriteria();
+				ViewCriteriaRow group1 = new ViewCriteriaRow();
+
+				for (int i = 0; i < filterList.size(); i++) 
+				{
+					ViewCriteriaItem item1 = new ViewCriteriaItem();
+
+					if ("ISBLANK".equals(filterList.get(i).get("itemOperator"))) 
+					{
+						item1.setAttribute((String) filterList.get(i).get("itemAttribute"));
+						item1.setUpperCaseCompare((boolean) filterList.get(i).get("upperCaseCompare"));
+						item1.setOperator((String) filterList.get(i).get("itemOperator"));
+					}
+					else if("BETWEEN".equals(filterList.get(i).get("itemOperator")))
+					{
+						item1.setAttribute((String) filterList.get(i).get("itemAttribute"));
+						item1.setUpperCaseCompare((boolean) filterList.get(i).get("upperCaseCompare"));
+						item1.setOperator((String) filterList.get(i).get("itemOperator"));
+						
+						for(String tmp : (String[]) filterList.get(i).get("itemValue")) {
+							item1.getValue().add(tmp);
+						}
+					}
+					else 
+					{
+						item1.setAttribute((String) filterList.get(i).get("itemAttribute"));
+						item1.getValue().add((String) filterList.get(i).get("itemValue"));
+						item1.setUpperCaseCompare((boolean) filterList.get(i).get("upperCaseCompare"));
+						item1.setOperator((String) filterList.get(i).get("itemOperator"));
+					}
+					
+					group1.getItem().add(item1);
+				}
+				
+				group1.setConjunction(conjunction);
+
+				filter.getGroup().add(group1);
+				findCriteria.setFilter(filter);
+			}
+		}
+
+		for (int i = 0; i < items.length; i++) {
+			findCriteria.getFindAttribute().add(items[i]);
+		}
+
+		return findCriteria;
+	}
+	
+	/**
+	 * 입력한 날짜가 없을 때 오늘 기준 3개월전 일자 구하는 함수
+	 * */
+	public String getFromDt() {
+		SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar today = Calendar.getInstance();
+        
+        String fromDt = "";
+        
+        today.set(Calendar.DATE, 1);        					// 현재달 1일
+        today.add(Calendar.MONTH, -3);						// 현재달 두달전
+        fromDt = dateForm.format(today.getTime());			// Data -> String convert
+        
+		return fromDt;
+	}
+
+	/**
+	 * 입력한 날짜가 있을 때 읿력 일자 기준 3개월전 일자 구하는 함수
+	 * */
+	public String getFromDt(String paramDt)  throws Exception{
+		SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd");
+		Date tmp = dateForm.parse(paramDt);
+		
+        Calendar today = Calendar.getInstance();
+        today.setTime(tmp);
+        
+        String fromDt = "";
+        
+        today.set(Calendar.DATE, 1);        					// 현재달 1일
+        today.add(Calendar.MONTH, -3);						// 현재달 두달전
+        fromDt = dateForm.format(today.getTime());			// Data -> String convert
+        
+		return fromDt;
+	}
+	
+	/**
+	 * 입력한 날짜가 없을 때 오늘 기준 전일 자 구하는 함수
+	 * */
+	public String getYesterday() 
+	{
+		SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar today = Calendar.getInstance();
+        
+        String yesterday = "";
+        
+        today.add(Calendar.DATE, -1);        					// 어제 날짜(day)
+        yesterday = dateForm.format(today.getTime());			// Data -> String convert
+        
+		return yesterday;
+	}
+	
+	/**
+	 * 입력한 날짜가 있을 때 입력 일자 기준 내일 자 구하는 함수
+	 * */
+	public String getTomorrow(String paramDt) throws Exception
+	{
+		SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd");
+        Date tmp = dateForm.parse(paramDt);
+		
+		Calendar today = Calendar.getInstance();
+		today.setTime(tmp);
+        
+        String tomorrowday = "";
+        
+        today.add(Calendar.DATE, 1);        					// 내일 날짜(day)
+        tomorrowday = dateForm.format(today.getTime());			// Data -> String convert
+        
+		return tomorrowday;
+	}
+	
+	/**
+	 * 입력한 날짜가 없을 때 오늘 기준 전일 자 구하는 함수
+	 * */
+	public String getToday() 
+	{
+		SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar today = Calendar.getInstance();
+        
+        String todayDt;
+        todayDt = dateForm.format(today.getTime());				// Data -> String convert
+        
+		return todayDt;
+	}
+	
+	/**
+	 * Null이거나 빈값(빈 문자열, 빈 컬렉션)인지 검사
+	 *
+	 * @param object
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public boolean isEmpty(Object object) 
+	{
+		if (object == null) {
+			return true;
+		} 
+		else if (object instanceof String) {
+			String str = (String) object;
+			return str.length() == 0;
+		} 
+		else if (object instanceof Collection) {
+			Collection collection = (Collection) object;
+			return collection.size() == 0;
+		}
+		else if (object instanceof Boolean) {
+			Boolean boolean2 = (Boolean) object;
+			return boolean2 == true;
+		}
+		else if (object.getClass().isArray()) {
+			try {
+				if (Array.getLength(object) == 0) {
+					return true;
+				}
+
+			} catch (Exception e) {
+				// do nothing
+			}
+		}
+
+		return false;
+	}
+
+	public String nullToZero(String str) {
+		if (str == null || "".equals(str)) {
+			str = "0";
+		}
+
+		return str;
+	}
+	
+	public String cutTxt(String szText, String szKey, int nLength, int nPrev, boolean isNotag, boolean isAdddot){  // 문자열 자르기
+	    String r_val = szText;
+	    int oF = 0, oL = 0, rF = 0, rL = 0;
+	    int nLengthPrev = 0;
+	    Pattern p = Pattern.compile("<(/?)([^<>]*)?>", Pattern.CASE_INSENSITIVE);  // 태그제거 패턴
+	   
+	    if(isNotag) {
+	    	r_val = p.matcher(r_val).replaceAll("");  // 태그 제거
+		    r_val = r_val.replaceAll("&amp;", "&");
+		    r_val = r_val.replaceAll("(!/|\r|\n|&nbsp;)", "");  // 공백제거
+	    }
+	 
+	    try {
+	      byte[] bytes = r_val.getBytes("UTF-8");     // 바이트로 보관     
+	      if(szKey != null && !szKey.equals("")) {
+	        nLengthPrev = (r_val.indexOf(szKey) == -1)? 0: r_val.indexOf(szKey);  // 일단 위치찾고
+	        nLengthPrev = r_val.substring(0, nLengthPrev).getBytes("MS949").length;  // 위치까지길이를 byte로 다시 구한다
+	        nLengthPrev = (nLengthPrev-nPrev >= 0)? nLengthPrev-nPrev:0;    // 좀 앞부분부터 가져오도록한다.
+	      }
+	        
+	      // x부터 y길이만큼 잘라낸다. 한글안깨지게.
+	      int j = 0;
+
+	      if(nLengthPrev > 0) while(j < bytes.length) {
+	        if((bytes[j] & 0x80) != 0) {
+	          oF+=2; rF+=3; if(oF+2 > nLengthPrev) {break;} j+=3;
+	        } else {if(oF+1 > nLengthPrev) {break;} ++oF; ++rF; ++j;}
+	      }
+	      
+	      j = rF;
+
+	      while(j < bytes.length) {
+	        if((bytes[j] & 0x80) != 0) {
+	          if(oL+2 > nLength) {break;} oL+=2; rL+=3; j+=3;
+	        } else {if(oL+1 > nLength) {break;} ++oL; ++rL; ++j;}
+	      }
+
+	      r_val = new String(bytes, rF, rL, "UTF-8");  // charset 옵션
+
+	      if(isAdddot && rF+rL+3 <= bytes.length) {r_val+="…";}  // …을 붙일지말지 옵션
+	    } catch(UnsupportedEncodingException e){ e.printStackTrace(); }  
+	    
+	    return r_val;
+	}
+
+	
+//	public String cutTxt(String txt, int maxByte){
+//		int cutByte = 0;
+//		int hangulByte = 3;
+//		int totalHangul = 0;
+//		String resultTxt = null;
+//		try {
+//			byte[] inputByte = txt.getBytes();
+//			for(int i = 0; i < txt.length(); i++){
+//				logger.info("txt.substring(i, i+1) : "+txt.substring(i, i+1));
+//				if(isIncludeHangul(txt.substring(i, i+1))){
+//					if(cutByte + hangulByte > maxByte){
+//						break;
+//					}
+//					cutByte += hangulByte;
+//					totalHangul += hangulByte;
+//				}else{
+//					if(cutByte + 1 > maxByte){
+//						break;
+//					}
+//					cutByte += 1;
+//				}
+//			}
+//			logger.info(cutByte + " byte 추출");
+//			logger.debug("한글 수 : " + totalHangul/3);
+//		
+//			resultTxt = new String(inputByte, 0 , cutByte, "UTF-8");
+//		} catch (UnsupportedEncodingException e) {
+//			// TODO Auto-generated catch block
+//			logger.info(e.toString());
+//		}
+//		
+//		logger.debug("resultTxt : " + resultTxt);
+//		
+//		return resultTxt;
+//	}
+	
+//	public boolean isIncludeHangul(String input){
+//		for(int i = 0; i < input.length(); i++){
+//			if(Character.getType(input.charAt(0))==Character.OTHER_LETTER){
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+
+}
---
+++ DliveBatch_dev/src/vo/ActivityVO.java
@@ -0,0 +1,256 @@
+package vo;
+
+public class ActivityVO {
+	
+	private String activityId;
+	private String activityNumber;
+	private String accountId;
+	private String accountName;
+	private String activityFunctioncode;
+	private String activityTypeCode;
+	private String activityStartDate;
+	private String activityEndDate;
+	private String ownerId;
+	private String ownerName;
+	private String ownerEmailAddress;
+	private String subject;
+	private String activityDescription;
+	private String leadId;
+	private String leadName;
+	private String opportunityId;
+	private String opportunityName;
+	private String primaryContactId;
+	private String primaryContactName;
+	private String priorityCode;
+	private String visitResult_c;
+	private String visitDate_c;
+	private String initialDate_c;
+	private String initialDateF_c;
+	private String initialDateTextF_c;
+	private String createdBy;
+	private String creationDate;
+	private String lastUpdateDate;
+	private String lastUpdatedBy;
+	private String actBranch_c;
+	private String privateFlag;
+	private String dliveActType;
+	private String batchJobId;
+	
+	
+	public String getDliveActType() {
+		return dliveActType;
+	}
+	public void setDliveActType(String dliveActType) {
+		this.dliveActType = dliveActType;
+	}
+	public String getPrivateFlag() {
+		return privateFlag;
+	}
+	public void setPrivateFlag(String privateFlag) {
+		this.privateFlag = privateFlag;
+	}
+	public String getActBranch_c() {
+		return actBranch_c;
+	}
+	public void setActBranch_c(String actBranch_c) {
+		this.actBranch_c = actBranch_c;
+	}
+	public String getActivityId() {
+		return activityId;
+	}
+	public void setActivityId(String activityId) {
+		this.activityId = activityId;
+	}
+	public String getActivityNumber() {
+		return activityNumber;
+	}
+	public void setActivityNumber(String activityNumber) {
+		this.activityNumber = activityNumber;
+	}
+	public String getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public String getActivityFunctioncode() {
+		return activityFunctioncode;
+	}
+	public void setActivityFunctioncode(String activityFunctioncode) {
+		this.activityFunctioncode = activityFunctioncode;
+	}
+	public String getActivityTypeCode() {
+		return activityTypeCode;
+	}
+	public void setActivityTypeCode(String activityTypeCode) {
+		this.activityTypeCode = activityTypeCode;
+	}
+	public String getActivityStartDate() {
+		return activityStartDate;
+	}
+	public void setActivityStartDate(String activityStartDate) {
+		this.activityStartDate = activityStartDate;
+	}
+	public String getActivityEndDate() {
+		return activityEndDate;
+	}
+	public void setActivityEndDate(String activityEndDate) {
+		this.activityEndDate = activityEndDate;
+	}
+	public String getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(String ownerId) {
+		this.ownerId = ownerId;
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+	public String getOwnerEmailAddress() {
+		return ownerEmailAddress;
+	}
+	public void setOwnerEmailAddress(String ownerEmailAddress) {
+		this.ownerEmailAddress = ownerEmailAddress;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getActivityDescription() {
+		return activityDescription;
+	}
+	public void setActivityDescription(String activityDescription) {
+		this.activityDescription = activityDescription;
+	}
+	public String getLeadId() {
+		return leadId;
+	}
+	public void setLeadId(String leadId) {
+		this.leadId = leadId;
+	}
+	public String getLeadName() {
+		return leadName;
+	}
+	public void setLeadName(String leadName) {
+		this.leadName = leadName;
+	}
+	public String getOpportunityId() {
+		return opportunityId;
+	}
+	public void setOpportunityId(String opportunityId) {
+		this.opportunityId = opportunityId;
+	}
+	public String getOpportunityName() {
+		return opportunityName;
+	}
+	public void setOpportunityName(String opportunityName) {
+		this.opportunityName = opportunityName;
+	}
+	public String getPrimaryContactId() {
+		return primaryContactId;
+	}
+	public void setPrimaryContactId(String primaryContactId) {
+		this.primaryContactId = primaryContactId;
+	}
+	public String getPrimaryContactName() {
+		return primaryContactName;
+	}
+	public void setPrimaryContactName(String primaryContactName) {
+		this.primaryContactName = primaryContactName;
+	}
+	public String getPriorityCode() {
+		return priorityCode;
+	}
+	public void setPriorityCode(String priorityCode) {
+		this.priorityCode = priorityCode;
+	}
+	public String getVisitResult_c() {
+		return visitResult_c;
+	}
+	public void setVisitResult_c(String visitResult_c) {
+		this.visitResult_c = visitResult_c;
+	}
+	public String getVisitDate_c() {
+		return visitDate_c;
+	}
+	public void setVisitDate_c(String visitDate_c) {
+		this.visitDate_c = visitDate_c;
+	}
+	public String getInitialDate_c() {
+		return initialDate_c;
+	}
+	public void setInitialDate_c(String initialDate_c) {
+		this.initialDate_c = initialDate_c;
+	}
+	public String getInitialDateF_c() {
+		return initialDateF_c;
+	}
+	public void setInitialDateF_c(String initialDateF_c) {
+		this.initialDateF_c = initialDateF_c;
+	}
+	public String getInitialDateTextF_c() {
+		return initialDateTextF_c;
+	}
+	public void setInitialDateTextF_c(String initialDateTextF_c) {
+		this.initialDateTextF_c = initialDateTextF_c;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(String creationDate) {
+		this.creationDate = creationDate;
+	}
+	public String getLastUpdateDate() {
+		return lastUpdateDate;
+	}
+	public void setLastUpdateDate(String lastUpdateDate) {
+		this.lastUpdateDate = lastUpdateDate;
+	}
+	public String getLastUpdatedBy() {
+		return lastUpdatedBy;
+	}
+	public void setLastUpdatedBy(String lastUpdatedBy) {
+		this.lastUpdatedBy = lastUpdatedBy;
+	}
+	public String getBatchJobId() {
+		return batchJobId;
+	}
+	public void setBatchJobId(String batchJobId) {
+		this.batchJobId = batchJobId;
+	}
+	@Override
+	public String toString() {
+		return "ActivityVO [activityId=" + activityId + ", activityNumber=" + activityNumber + ", accountId="
+				+ accountId + ", accountName=" + accountName + ", activityFunctioncode=" + activityFunctioncode
+				+ ", activityTypeCode=" + activityTypeCode + ", activityStartDate=" + activityStartDate
+				+ ", activityEndDate=" + activityEndDate + ", ownerId=" + ownerId + ", ownerName=" + ownerName
+				+ ", ownerEmailAddress=" + ownerEmailAddress + ", subject=" + subject + ", activityDescription="
+				+ activityDescription + ", leadId=" + leadId + ", leadName=" + leadName + ", opportunityId="
+				+ opportunityId + ", opportunityName=" + opportunityName + ", primaryContactId=" + primaryContactId
+				+ ", primaryContactName=" + primaryContactName + ", priorityCode=" + priorityCode + ", visitResult_c="
+				+ visitResult_c + ", visitDate_c=" + visitDate_c + ", initialDate_c=" + initialDate_c
+				+ ", initialDateF_c=" + initialDateF_c + ", initialDateTextF_c=" + initialDateTextF_c + ", createdBy="
+				+ createdBy + ", creationDate=" + creationDate + ", lastUpdateDate=" + lastUpdateDate
+				+ ", lastUpdatedBy=" + lastUpdatedBy + ", actBranch_c=" + actBranch_c + ", privateFlag=" + privateFlag
+				+ ", dliveActType=" + dliveActType + ", batchJobId=" + batchJobId + "]";
+	}
+	
+	
+}
